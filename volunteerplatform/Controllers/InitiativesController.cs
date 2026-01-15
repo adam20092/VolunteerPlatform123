215b5a27616b065d8ea0aaa9b828aa2dadc89d4f
adam20092 <akasabi2009@gmail.com>
2026-01-15 11:04:27 +0200

    rating cntrl

--- volunteerplatform/Controllers/InitiativesController.cs
+++ volunteerplatform/Controllers/InitiativesController.cs
@@ -1,24 +1,86 @@
-using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using volunteerplatform.Data;
 using volunteerplatform.Models;
 
-namespace volunteerplatform.Controllers;
-
-public class HomeController : Controller
+namespace volunteerplatform.Controllers
 {
-    public IActionResult Index()
+    public class InitiativesController : Controller
     {
-        return View();
-    }
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        public InitiativesController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        // GET: Initiatives
+        public async Task<IActionResult> Index()
+        {
+            var initiatives = await _context.Initiatives.Include(i => i.Organizer).ToListAsync();
+            return View(initiatives);
+        }
+
+        // GET: Initiatives/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var initiative = await _context.Initiatives
+                .Include(i => i.Organizer)
+                .Include(i => i.Enrolments).ThenInclude(e => e.Volunteer)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (initiative == null)
+            {
+                return NotFound();
+            }
+
+            return View(initiative);
+        }
+
+        // GET: Initiatives/Create
+        [Authorize(Roles = "Organizer,Admin")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Initiatives/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Organizer,Admin")]
+        public async Task<IActionResult> Create([Bind("Id,Title,Description,Location,Latitude,Longitude,DateAndTime,RequiredVolunteers,RequiredSkills")] Initiative initiative)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.GetUserAsync(User);
+                initiative.OrganizerId = user.Id;
+                initiative.Status = MissionStatus.Active;
+
+                _context.Add(initiative);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(initiative);
+        }
+
+        // GET: Initiatives/MyInitiatives
+        [Authorize(Roles = "Organizer")]
+        public async Task<IActionResult> MyInitiatives()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            var myInitiatives = await _context.Initiatives
+                .Where(i => i.OrganizerId == user.Id)
+                .ToListAsync();
+            return View("Index", myInitiatives);
+        }
     }
 }
--- volunteerplatform/Controllers/RatingsController.cs
+++ volunteerplatform/Controllers/RatingsController.cs
@@ -1,86 +1,50 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using volunteerplatform.Data;
 using volunteerplatform.Models;
 
 namespace volunteerplatform.Controllers
 {
-    public class InitiativesController : Controller
+    [Authorize]
+    public class RatingsController : Controller
     {
         private readonly ApplicationDbContext _context;
         private readonly UserManager<ApplicationUser> _userManager;
 
-        public InitiativesController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        public RatingsController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
             _userManager = userManager;
         }
 
-        // GET: Initiatives
-        public async Task<IActionResult> Index()
+        [HttpGet]
+        public IActionResult Rate(int initiativeId, string volunteerId)
         {
-            var initiatives = await _context.Initiatives.Include(i => i.Organizer).ToListAsync();
-            return View(initiatives);
-        }
-
-        // GET: Initiatives/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
+            var model = new Rating
             {
-                return NotFound();
-            }
-
-            var initiative = await _context.Initiatives
-                .Include(i => i.Organizer)
-                .Include(i => i.Enrolments).ThenInclude(e => e.Volunteer)
-                .FirstOrDefaultAsync(m => m.Id == id);
-
-            if (initiative == null)
-            {
-                return NotFound();
-            }
-
-            return View(initiative);
+                InitiativeId = initiativeId,
+                VolunteerId = volunteerId
+            };
+            return View(model);
         }
 
-        // GET: Initiatives/Create
-        [Authorize(Roles = "Organizer,Admin")]
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Initiatives/Create
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        [Authorize(Roles = "Organizer,Admin")]
-        public async Task<IActionResult> Create([Bind("Id,Title,Description,Location,Latitude,Longitude,DateAndTime,RequiredVolunteers,RequiredSkills")] Initiative initiative)
+        public async Task<IActionResult> Rate(Rating rating)
         {
-            if (ModelState.IsValid)
+            if (rating.Score < 1 || rating.Score > 5)
             {
-                var user = await _userManager.GetUserAsync(User);
-                initiative.OrganizerId = user.Id;
-                initiative.Status = MissionStatus.Active;
-
-                _context.Add(initiative);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                 ModelState.AddModelError("", "Score must be between 1 and 5");
+                 return View(rating);
             }
-            return View(initiative);
-        }
 
-        // GET: Initiatives/MyInitiatives
-        [Authorize(Roles = "Organizer")]
-        public async Task<IActionResult> MyInitiatives()
-        {
             var user = await _userManager.GetUserAsync(User);
-            var myInitiatives = await _context.Initiatives
-                .Where(i => i.OrganizerId == user.Id)
-                .ToListAsync();
-            return View("Index", myInitiatives);
+            rating.OrganizerId = user.Id;
+
+            _context.Ratings.Add(rating);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Manage", "Enrolments", new { id = rating.InitiativeId });
         }
     }
 }
