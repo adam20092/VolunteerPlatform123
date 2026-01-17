befb072e03dadaadef19ab8eb410149f92627481
adam20092 <akasabi2009@gmail.com>
2026-01-17 09:26:55 +0200

    Dokumentaciq

--- volunteerplatform/obj/Debug/net10.0/volunteerplatform.AssemblyInfo.cs
+++ volunteerplatform/obj/Debug/net10.0/volunteerplatform.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("volunteerplatform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5243894bf935e8d9def9b60ddd0f24cc816da009")]
 [assembly: System.Reflection.AssemblyProductAttribute("volunteerplatform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("volunteerplatform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
