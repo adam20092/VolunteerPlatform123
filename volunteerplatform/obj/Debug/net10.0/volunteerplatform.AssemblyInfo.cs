5354791858ef71f320869568d0355372a750380c
adam20092 <akasabi2009@gmail.com>
2026-02-12 10:43:40 +0200

    button functionality

--- volunteerplatform/obj/Debug/net10.0/volunteerplatform.AssemblyInfo.cs
+++ volunteerplatform/obj/Debug/net10.0/volunteerplatform.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("volunteerplatform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+befb072e03dadaadef19ab8eb410149f92627481")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+815e70370229b2ade131342536788999d6edf50f")]
 [assembly: System.Reflection.AssemblyProductAttribute("volunteerplatform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("volunteerplatform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
