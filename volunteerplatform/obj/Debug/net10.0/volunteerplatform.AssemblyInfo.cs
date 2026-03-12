e2fc866c83f23a210367fbefd06b886603c1db8d
adam20092 <akasabi2009@gmail.com>
2026-03-12 08:40:27 +0200

    build: Add generated debug build artifacts for the VolunteerPlatform project.

--- volunteerplatform/obj/Debug/net10.0/volunteerplatform.AssemblyInfo.cs
+++ volunteerplatform/obj/Debug/net10.0/volunteerplatform.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("volunteerplatform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d851ee525d442e6b0e42fa02419006d1b5c5f312")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d41abe3d9d3c298c71d70f8ef26ef99bd9e5193")]
 [assembly: System.Reflection.AssemblyProductAttribute("volunteerplatform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("volunteerplatform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
