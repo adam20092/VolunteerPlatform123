13cb2baf1938b5179b5bf23c4fc1fd1cc5155904
adam20092 <akasabi2009@gmail.com>
2026-03-27 12:29:42 +0200

    feat: Add initial Home Index and Initiatives Create views.

--- volunteerplatform/obj/Debug/net10.0/volunteerplatform.AssemblyInfo.cs
+++ volunteerplatform/obj/Debug/net10.0/volunteerplatform.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("volunteerplatform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83894053d84dbf60d2056b727552c13b022b1cd7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41a8b99ed1b489bf19350b83cbb16b7a60449cf0")]
 [assembly: System.Reflection.AssemblyProductAttribute("volunteerplatform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("volunteerplatform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
