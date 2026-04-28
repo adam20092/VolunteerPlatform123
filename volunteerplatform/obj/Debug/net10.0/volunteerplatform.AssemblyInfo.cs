3effd88a2c20ce7f80661c6e57a711f10fea1bd1
adam20092 <akasabi2009@gmail.com>
2026-04-28 23:25:41 +0300

    build: generate project build artifacts and dependency caches

--- volunteerplatform/obj/Debug/net10.0/volunteerplatform.AssemblyInfo.cs
+++ volunteerplatform/obj/Debug/net10.0/volunteerplatform.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("volunteerplatform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68aee870b0dcbf0038c7825a71329a27b6764044")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e6522f43b665b3ef18df82f7423e13a03ea17f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("volunteerplatform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("volunteerplatform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
